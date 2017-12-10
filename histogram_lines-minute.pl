84deeeb9fd9fb4156ac996808bf9e5eb1fb05782
Allen Mao <excursion.allen@gmail.com>
2017-12-09 19:16:25 -0800

    09Dec2017 1916

--- histogram_lines-minute.pl
+++ histogram_lines-minute.pl
@@ -2,4 +2,11 @@
 #histgram_lines-minute.pl
 
 use strict; use warnings;
+use Subtitles;
+die "Usage $0 <file.srt>" if (@ARGV != 1);
+my ($file) = @ARGV;
+open (my $fh, "<", $file) or die "Cannot open < $file: $!";
+my $subtitle = Subtitles->new();
+die "Cannot load:$@\n" unless $sub->load($fh);
+close ($fh);
 
--- histogram_lines-minute.py
+++ histogram_lines-minute.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3
 # histogram_lines-minute.py
-print("Helo World")
+import sys
+import pysrt
+if (len(sys.argv) != 2):
+    raise Exception("Incorrect input")
+filename = sys.argv[1]
+#For the above command is there a perl-like operation like
+#my ($filename) = @ARGV; ?
+
+#In this first implementation, count based on starting times
+
+subs = pysrt.open(filename)
+sub_count = {}
+for sub in subs:
+    #print("Start", sub.start.minutes, "\t", sub.end.minutes, "\n")
+    sub_count[sub.start.minutes] += 1
+
+for
